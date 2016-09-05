9e7ba0a37864f2b0507ab11fd2f8f8449683906f
fakeezz <fakeezz@gmail.com>
2016-09-05 15:19:10 -0300

    sort ssh tunnels

--- fish/omf/pkg/ssh/functions/ssh-tunnel-list.fish
+++ fish/omf/pkg/ssh/functions/ssh-tunnel-list.fish
@@ -1,5 +1,5 @@
 function ssh-tunnel-list -d "List all ssh-tunnel configured"
-    cat $OMF_SSH_TUNNEL_DB |awk -F';' '{print $1}'
+    cat $OMF_SSH_TUNNEL_DB |awk -F';' '{print $1}'| sort
     return 0
 end
 
