adead2f33c372ac5dc75f49d59ffa955dd9512e5
fakeezz <fakeezz@gmail.com>
2016-09-14 11:26:46 -0300

    update configure_fish to set new paths

--- fish/configure_fish.fish
+++ fish/configure_fish.fish
@@ -1,12 +1,13 @@
 #!env /usr/bin/fish
 
-if not [ -d ~/.oh-my-fish ]
+if not [ -d ~/.local.share/omf ]
     echo "Cloning oh-my-fish"
     curl -L https://github.com/fakeezz/oh-my-fish/raw/master/bin/install | fish
 end
 
 echo "Installing fish configurations"
 
+rm -rf ~/.config/omf
 ln -s $PWD/omf ~/.config/omf
 
 sleep 3
