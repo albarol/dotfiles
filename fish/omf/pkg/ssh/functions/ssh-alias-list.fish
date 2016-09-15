7edc9756660f99061533c56ab7c2ddc8367052f6
fakeezz <fakeezz@gmail.com>
2016-09-15 20:00:40 -0300

    added support to ssh-alias

---
+++ fish/omf/pkg/ssh/functions/ssh-alias-list.fish
@@ -0,0 +1,3 @@
+function ssh-alias-list -d "Show all ssh alias"
+    settings-list $OMF_SSH_ALIAS_DB
+end
--- fish/omf/pkg/ssh/init.fish
+++ fish/omf/pkg/ssh/init.fish
@@ -1,4 +1,5 @@
 function init -a path --on-event init_ssh
     set -gx OMF_SSH_TUNNEL_DB ~/.config/.omf_ssh_tunnel.db
+    set -gx OMF_SSH_ALIAS_DB ~/.config/.omf_ssh_alias.db
 end
 
