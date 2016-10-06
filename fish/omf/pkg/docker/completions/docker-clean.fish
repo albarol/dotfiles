711762e767cb60bcee9f92b244d6df8dd7930599
fakeezz <fakeezz@gmail.com>
2016-10-06 17:30:31 -0300

    add completions

---
+++ fish/omf/pkg/docker/completions/docker-clean.fish
@@ -0,0 +1,3 @@
+complete -c docker-clean -a "week" -d "Remove containers not used on last weeks"
+complete -c docker-clean -a "year" -d "Remove containers not used on last years"
+complete -c docker-clean -a "month" -d "Remove containers not used on last months"
---
+++ fish/omf/pkg/docker/completions/docker-stop.fish
@@ -0,0 +1,2 @@
+complete -c docker-stop -a "running" -d "Stop all running containers"
+complete -c docker-stop -a "all" -d "Stop all containers"
