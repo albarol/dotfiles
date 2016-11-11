df05e6be12597f6f686d55ba4dacf55b2b23e390
fakeezz <fakeezz@gmail.com>
2016-11-11 16:26:52 -0200

    Add function to run a specific command against csv file

---
+++ fish/omf/pkg/common/functions/runcsv.fish
@@ -0,0 +1,19 @@
+
+function runcsv -d "Run commands on csv file"
+
+    if [ (count $argv) -ne 2 ]
+       echo "\
+Usage: runcsv "(omf::em)"<csvfile> <command>"(omf::off)
+        return $OMF_MISSING_ARG
+    end
+
+    set filename $argv[1]
+    set command $argv[2]
+
+    if not [ -e $filename ]
+        echo (omf::err)"File $argv[1] not found."(omf::off)
+        return $OMF_INVALID_ARG
+    else
+        cat $filename | tr ',' ' ' | xargs -l bash -c $command
+    end
+end
