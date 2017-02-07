66c1324411307cc7e9aa06eb631f344b9770c95a
fakeezz <fakeezz@gmail.com>
2017-02-07 16:54:57 -0200

    Set up common global variables

--- fish/omf/pkg/common/init.fish
+++ fish/omf/pkg/common/init.fish
@@ -1,11 +1,24 @@
 function init -a path --on-event init_common
     # GO PATH
-    set -gx PATH /usr/local/go/bin $PATH
-    set -gx GOPATH $HOME/.go
+    if [ -d /usr/local/go/bin ]
+        set -gx PATH /usr/local/go/bin $PATH
+        set -gx GOPATH $HOME/.go
+    end
 
     # ANDROID PATH
-    set -gx ANDROID_HOME $HOME/.android
-    set -gx ANDROID_SDK_HOME /opt/android-sdk-linux
-    set -gx PATH $ANDROID_SDK_HOME/tools/ $ANDROID_SDK_HOME/platform-tools/ $PATH
+    if [ -d $HOME/.android-sdk ]
+        set -gx ANDROID_SDK_HOME $HOME/.android-sdk
+        set -gx ANDROID_HOME $ANDROID_SDK_HOME/.android
+        set -gx PATH $ANDROID_SDK_HOME/tools/ $ANDROID_SDK_HOME/platform-tools/ $PATH
+    end
 
+    # NPM PATH
+    if [ -d $HOME/.npm-global ]
+        set -gx PATH $HOME/.npm-global $PATH
+    end
+
+    # JAVA PATH
+    if [ -d /opt/android-studio/jre ]
+        set -gx JAVA_HOME /opt/android-studio/jre
+    end
 end
