41ca13c17c076f25783559534a753b151cd1d21f
fakeezz <fakeezz@gmail.com>
2017-10-08 18:17:25 -0300

    Migrate fish utilities to zsh

---
+++ zsh/.zshrc
@@ -0,0 +1,23 @@
+# Path to your oh-my-zsh installation.
+export ZSH=$HOME/.oh-my-zsh
+
+# Theme
+export TERM="xterm-256color"
+ZSH_THEME="bira"
+
+# Change the command execution time stamp shown in the history command output.
+HIST_STAMPS="yyyy-mm-dd"
+
+# Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
+plugins=(git compleat history-search-multi-word zsh-autosuggestions)
+
+source $ZSH/oh-my-zsh.sh
+
+# You may need to manually set your language environment
+export LANG=en_US.UTF-8
+
+# Preferred editor for local and remote sessions
+export EDITOR='vim'
+
+# Path to custom scripts
+export PATH=$PATH:$HOME/.zsh/bin
--- zsh/configure_zsh.sh
+++ zsh/configure_zsh.sh
@@ -1,14 +1,44 @@
 #!/usr/bin/env bash
 
-if [ ! -x /usr/bin/zsh ]; then
-    echo "Installing ZSH"
-    sudo apt-get update
-    sudo apt-get install zsh git curl
-    chsh -s `which zsh`
-fi
+install_zsh() {
+    if [ ! -x /usr/bin/zsh ]; then
+        echo "Installing ZSH"
+        sudo apt-get update
+        sudo apt-get install zsh git curl
+        chsh -s `which zsh`
+    fi
+}
 
-if [ ! -d ~/.oh-my-zsh ]; then
-    echo "Installing OH-MY-ZSH"
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-fi
+install_oh_my_zsh() {
+    if [ ! -d ~/.oh-my-zsh ]; then
+        echo "Installing OH-MY-ZSH"
+        sh -c "$(curl -fsSL https://raw.githubusercontent.com/fakeezz/oh-my-zsh/master/tools/install.sh)"
+    fi
+}
 
+configure_zsh() {
+    SOURCE="${BASH_SOURCE[0]}"
+    while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+        DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+        SOURCE="$(readlink "$SOURCE")"
+        [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+    done
+    DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+    echo "Configuring ZSH"
+
+    # Replace default config file
+    if [[ -e $HOME/.zshrc ]]; then
+        rm $HOME/.zshrc
+    fi
+    ln -s $DIR/.zshrc $HOME/.zshrc
+
+    if [[ ! -d $HOME/.zsh ]]; then
+        mkdir $HOME/.zsh
+    fi
+    ln -s $DIR/../bin $HOME/.zsh/bin
+}
+
+install_zsh
+install_oh_my_zsh
+configure_zsh
